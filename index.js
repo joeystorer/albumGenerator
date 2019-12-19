e813f29873773554322218872cd94cd8e5822349
joeystorer <joeystorer@users.noreply.github.com>
2019-12-19 13:20:40 -0500

    update API key

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 getNames();
 
 function getNames() {
-    $.get("https://random-word-api.herokuapp.com//word?key=HQ70QA9M&number=2", function(res) {
+    $.get("https://random-word-api.herokuapp.com//word?key=512WE8JN&number=2", function(res) {
         $(".artist-name").text(res[0]);
         $(".album-title").text(res[1]);
     })
